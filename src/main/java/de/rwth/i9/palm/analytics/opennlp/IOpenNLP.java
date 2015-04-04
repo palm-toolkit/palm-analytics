f2fca1f902ad6412a520403060fa34af348971dc
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-04-04 13:43:00 +0200

    updated open nlp implementation

--- src/main/java/de/rwth/i9/palm/analytics/opennlp/IOpenNLP.java
+++ src/main/java/de/rwth/i9/palm/analytics/opennlp/IOpenNLP.java
@@ -3,14 +3,38 @@ package de.rwth.i9.palm.analytics.opennlp;
 public interface IOpenNLP
 {
 	/**
-	 * Break the given content into sentences.
+	 * Break the given content/corpus into sentences.
 	 * <p>
 	 * The sentence detector is lazily initialized on first use.
 	 * </p>
 	 * 
 	 * @param content
-	 *            the content to break into sentences
-	 * @return the detected sentences
+	 *            the text corpus
+	 * @return the detected sentences in string array
 	 */
 	public String[] detectSentences( final String content );
+
+	/**
+	 * Tokenize the given sentence.
+	 * <p>
+	 * The tokenizer is lazily initialized on first use.
+	 * </p>
+	 * 
+	 * @param sentence
+	 *            a sentence to tokenize
+	 * @return the individual tokens
+	 */
+	public String[] tokenize( final String sentence );
+
+	/**
+	 * Detect the part of speech tags for the given tokens in a sentence.
+	 * <p>
+	 * The tagger is lazily initialized on first use.
+	 * </p>
+	 * 
+	 * @param tokens
+	 *            an array of sentence tokens to tag
+	 * @return the individual part-of-speech tags
+	 */
+	public String[] tagPartOfSpeech( final String[] tokens );
 }
--- src/main/java/de/rwth/i9/palm/analytics/opennlp/OpenNLPImpl.java
+++ src/main/java/de/rwth/i9/palm/analytics/opennlp/OpenNLPImpl.java
@@ -2,10 +2,21 @@ package de.rwth.i9.palm.analytics.opennlp;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
+import opennlp.tools.coref.Linker;
+import opennlp.tools.namefind.TokenNameFinder;
+import opennlp.tools.parser.Parser;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTagger;
+import opennlp.tools.postag.POSTaggerME;
 import opennlp.tools.sentdetect.SentenceDetector;
 import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,34 +29,38 @@ import de.rwth.i9.palm.analytics.service.ConfigurationService;
 public class OpenNLPImpl implements IOpenNLP
 {
 	static private final Logger logger = LoggerFactory.getLogger( OpenNlpToolkit.class );
-
+	/**
+	 * application configuration
+	 */
 	@Autowired
 	private ConfigurationService configurationService;
 
-	/**
-	 * OpenNLP components, lazily initialized.
-	 * <ul>
-	 * <li>{@link #detectSentences(String)}</li>
-	 * </ul>
-	 */
-	private SentenceDetector _sentenceDetector = null;
+	// the named entities
+	private static final String[] NAME_TYPES = { "person", "organization", "location" };
+
+	// OpenNLP components, lazily initialized.
+	private SentenceDetector sentenceDetector = null;
+	private Tokenizer tokenizer = null;
+	private POSTagger posTagger = null;
+	final private Map<String, TokenNameFinder> _nameFinderMap = new HashMap<String, TokenNameFinder>();
+	private Parser _parser = null;
+	private Linker _linker = null;
 
 	@Override
 	public String[] detectSentences( String content )
 	{
-		if ( _sentenceDetector == null )
+		if ( sentenceDetector == null )
 		{
 			// lazy initialize
 			InputStream modelIn = null;
 			try
 			{
 				// sentence detector
-				final String location = configurationService.getopennlpSentence();
-				logger.info( "Loading sentence detection model: " + location );
-				modelIn = getClass().getResourceAsStream( location );
+				logger.info( "Loading sentence detection model" );
+				modelIn = getClass().getResourceAsStream( configurationService.getOpenNLPSentence() );
 				final SentenceModel sentenceModel = new SentenceModel( modelIn );
 				modelIn.close();
-				_sentenceDetector = new SentenceDetectorME( sentenceModel );
+				sentenceDetector = new SentenceDetectorME( sentenceModel );
 				logger.info( "done." );
 			}
 			catch ( final IOException ioe )
@@ -68,7 +83,83 @@ public class OpenNLPImpl implements IOpenNLP
 		}
 
 		// detect sentences
-		return _sentenceDetector.sentDetect( content );
+		return sentenceDetector.sentDetect( content );
+	}
+
+	@Override
+	public String[] tokenize( String sentence )
+	{
+		if ( tokenizer == null )
+		{
+			// lazy initialize
+			InputStream modelIn = null;
+			try
+			{
+				// tokenizer
+				logger.info( "Loading tokenizer model" );
+				modelIn = getClass().getResourceAsStream( configurationService.getOpenNLPTokenizer() );
+				final TokenizerModel tokenModel = new TokenizerModel( modelIn );
+				modelIn.close();
+				tokenizer = new TokenizerME( tokenModel );
+				logger.info( "done." );
+			}
+			catch ( final IOException ioe )
+			{
+				logger.error( "Error loading tokenizer", ioe );
+			}
+			finally
+			{
+				if ( modelIn != null )
+				{
+					try
+					{
+						modelIn.close();
+					}
+					catch ( final IOException e )
+					{
+					}
+				}
+			}
+		}
+		return tokenizer.tokenize( sentence );
+	}
+
+	@Override
+	public String[] tagPartOfSpeech( String[] tokens )
+	{
+		if ( posTagger == null )
+		{
+			// lazy initialize
+			InputStream modelIn = null;
+			try
+			{
+				// tagger
+				logger.info( "Loading part-of-speech model" );
+				modelIn = getClass().getResourceAsStream( configurationService.getOpenNLPPos() );
+				final POSModel posModel = new POSModel( modelIn );
+				modelIn.close();
+				posTagger = new POSTaggerME( posModel );
+				logger.info( "done." );
+			}
+			catch ( final IOException ioe )
+			{
+				logger.error( "Error loading part-of-speech tagger", ioe );
+			}
+			finally
+			{
+				if ( modelIn != null )
+				{
+					try
+					{
+						modelIn.close();
+					}
+					catch ( final IOException e )
+					{
+					}
+				}
+			}
+		}
+		return posTagger.tag( tokens );
 	}
 
 }
