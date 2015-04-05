b7b1c2898231d7a28e3d69e2a6e02967d57c435e
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-04-05 22:20:51 +0200

    updated test cases and remove old test

--- src/test/java/de/rwth/i9/palm/analytics/algorithm/cvalue/CValueTest.java
+++ src/test/java/de/rwth/i9/palm/analytics/algorithm/cvalue/CValueTest.java
@@ -1,78 +1,62 @@
 package de.rwth.i9.palm.analytics.algorithm.cvalue;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertThat;
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
+import de.rwth.i9.palm.analytics.api.PalmAnalytics;
+import de.rwth.i9.palm.analytics.config.AppConfig;
+
+/**
+ * Test all functionalities of CValue algorithm through interface and classes
+ * 
+ */
+
+@RunWith( SpringJUnit4ClassRunner.class )
+@ContextConfiguration( classes = AppConfig.class, loader = AnnotationConfigContextLoader.class )
 public class CValueTest
 {
+	@Autowired
+	PalmAnalytics palmAnalytics;
 
-	@Test
-	public void test()
+	@Before
+	public void init()
 	{
-
-		List<String> phrases = new LinkedList<String>();
-
-		for ( int i = 0; i < 5; i++ )
-			phrases.add( "ADENOID CYSTIC BASAL CELL CARCINOMA" );
-
-		for ( int i = 0; i < 11; i++ )
-			phrases.add( "CYSTIC BASAL CELL CARCINOMA" );
-
-		for ( int i = 0; i < 7; i++ )
-			phrases.add( "ULCERATED BASAL CELL CARCINOMA" );
-
-		for ( int i = 0; i < 5; i++ )
-			phrases.add( "RECURRENT BASAL CELL CARCINOMA" );
-
-		for ( int i = 0; i < 3; i++ )
-			phrases.add( "CIRCUMSCRIBED BASAL CELL CARCINOMA" );
-
-		for ( int i = 0; i < 984; i++ )
-			phrases.add( "BASAL CELL CARCINOMA" );
-		
-		phrases.add( "CELL CARCINOMA" );
-
-		CValueSess cvals;
-		ArrayList<Candidate> candList;
-
-		cvals = new CValueSess();
-
-		for ( String phrase : phrases )
-		{
-
-			cvals.observe( phrase );
-		}
-
-		cvals.calculate();
-
-		candList = new ArrayList<Candidate>( cvals.getCandidates() );
-		Collections.sort( candList, new CValueComparatorOri() );
-
-		System.out.println( "Original" );
-
-		for ( Candidate cand : candList )
-		{
-			String resultLine = cand.getLength() + " " + cand.getFrequency() + "  " + cand.getNesterCount() + " " + cand.getFreqNested() + " " + cand.getString() + " " + cand.getCValue();
-			System.out.println( resultLine );
-		}
-
-		System.out.println();
+		assertThat( palmAnalytics, is( not( ( nullValue() ) ) ) );
 	}
 
 	@Test
-	@Ignore
 	public void testCandidateTermSubstring()
 	{
 		TermCandidate tc = new TermCandidate( "RECURRENT BASAL CELL CARCINOMA" );
 		List<String> tcSubTerm = tc.getTermCandidateSubString();
 
-		for ( String term : tcSubTerm )
-			System.out.println( term );
+		List<String> expectedSubTerm = new ArrayList<String>();
+		expectedSubTerm.add( "RECURRENT BASAL CELL" );
+		expectedSubTerm.add( "BASAL CELL CARCINOMA" );
+		expectedSubTerm.add( "RECURRENT BASAL" );
+		expectedSubTerm.add( "BASAL CELL" );
+		expectedSubTerm.add( "CELL CARCINOMA" );
+		expectedSubTerm.add( "RECURRENT" );
+		expectedSubTerm.add( "BASAL" );
+		expectedSubTerm.add( "CELL" );
+		expectedSubTerm.add( "CARCINOMA" );
+
+		assertThat( tcSubTerm, is( expectedSubTerm ) );
 	}
 
 	@Test
@@ -106,12 +90,27 @@ public class CValueTest
 		cva.setFrequencyThreshold( 2 );
 		cva.calculateCValue();
 
+		// because the threshold set to 2
+		// the "CELL CARCINOMA" should be ignored
+		// thus, there must be 6 types of multi-word terms
+		assertThat( "multi-word term size", cva.getTermCandidates().size(), is( 6 ) );
+
+		// the result should be
+		// length freq freqnested numbernester molti-word-term
+		// calculation-result
+		// 3 984 5 31 BASAL CELL CARCINOMA 1549.7763332051466
+		// 4 7 0 0 ULCERATED BASAL CELL CARCINOMA 14.0
+		// 4 11 1 5 CYSTIC BASAL CELL CARCINOMA 12.0
+		// 5 5 0 0 ADENOID CYSTIC BASAL CELL CARCINOMA 11.60964047443681
+		// 4 5 0 0 RECURRENT BASAL CELL CARCINOMA 10.0
+		// 4 3 0 0 CIRCUMSCRIBED BASAL CELL CARCINOMA 6.0
+		TermCandidate highestCandidate = null;
 		for ( TermCandidate cand : cva.getTermCandidates() )
 		{
-			String resultLine = cand.getCandidateLength() + " " + cand.getCandidateFrequencies() + "  " + cand.getUniqueNesterCandidates() + " " + cand.getCandidateNestedFrequencies() + " " + cand.getCandidateTerm() + " " + cand.getCValue();
-			System.out.println( resultLine );
+			highestCandidate = cand;
+			if ( highestCandidate != null )
+				break;
 		}
-		System.out.println();
+		assertThat( "largest candidate value ", highestCandidate.getCValue(), is( greaterThan( 1500.0 )) );
 	}
-
 }
