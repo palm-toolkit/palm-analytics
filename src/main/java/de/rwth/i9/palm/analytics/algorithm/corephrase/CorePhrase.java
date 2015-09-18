89c7818199bfdcc806e1f49fae491819fdc688f9
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-09-18 20:39:15 +0200

    Add short comment in CorePhrase Interface

--- src/main/java/de/rwth/i9/palm/analytics/algorithm/corephrase/CorePhrase.java
+++ src/main/java/de/rwth/i9/palm/analytics/algorithm/corephrase/CorePhrase.java
@@ -1,5 +1,11 @@
 package de.rwth.i9.palm.analytics.algorithm.corephrase;
 
+/**
+ * Currently, CorePhrase algorithm is implemented on palm-core module
+ * 
+ * @author sigit
+ *
+ */
 public interface CorePhrase
 {
 
