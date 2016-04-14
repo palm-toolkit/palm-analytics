1d1b869f672e1cc9ed7dcaf1f139618ae3d0443c
Sigit Nugraha <sigit.nugraha@gesis.org>
2016-04-14 03:23:17 +0200

    Add Levensthein distance on integer representation

--- src/main/java/de/rwth/i9/palm/analytics/textcompare/TextCompare.java
+++ src/main/java/de/rwth/i9/palm/analytics/textcompare/TextCompare.java
@@ -3,4 +3,6 @@ package de.rwth.i9.palm.analytics.textcompare;
 public interface TextCompare
 {
 	float getDistanceByLuceneLevenshteinDistance( String text1, String text2);
+
+	int getNumberCharacterDistanceByLevenshteinDistance( String text1, String text2 );
 }
